e0cb8482fa668f5a58d0c1c6ded69fd3d96eb42f
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-03-03 22:05:07 -0800

    updated

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    const timestamp = Date.now(); // Generate a unique timestamp
+
+    const qrFilename = `qr_${timestamp}.png`; // Unique filename
+    const textFilename = `URL_${timestamp}.txt`; // Unique text file
+
+    var qr_svg = qr.image(url, { type: "png" });
+    qr_svg.pipe(fs.createWriteStream(qrFilename));
+
+    fs.writeFile(textFilename, url, (err) => {
+      if (err) throw err;
+      console.log(`QR code saved as ${qrFilename}`);
+      console.log(`URL saved as ${textFilename}`);
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log("Something went wrong", error);
+    }
+  });
